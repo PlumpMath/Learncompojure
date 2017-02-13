1ee74ab17559af432ac01fd6324fbf8171ccbbd3
ThinhP.Tran <trphthinh@gmail.com>
2017-02-13 21:02:58 +0700

    Test several compojure functions

--- src/learncompojure/core.clj
+++ src/learncompojure/core.clj
@@ -1,6 +1,43 @@
-(ns learncompojure.core)
+(ns learncompojure.core
+  (:require [compojure.core :refer :all]
+            [org.httpkit.server :refer [run-server]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defroutes myapp
+  (GET "/" [] "Show something")
+  (POST "/" [] "Create something")
+  (PUT "/" [] "Replace something")
+  (PATCH "/" [] "Modify something")
+  (DELETE "/" [] "Annihilate something")
+  (OPTIONS "/" [] "Appease something")
+  (HEAD "/" [] "Preview something"))
+
+
+(myapp {:uri "/" :request-method :get})
+(myapp {:uri "/" :request-method :post})
+(myapp {:uri "/" :request-method :put})
+(myapp {:uri "/" :request-method :patch})
+(myapp {:uri "/" :request-method :delete})
+(myapp {:uri "/" :request-method :options})
+(myapp {:uri "/" :request-method :head})
+
+
+(defroutes myapp1
+  (GET "/hello/:name" [name] (str "Hello " name)))
+
+(myapp1 {:uri "/hello/Thinh" :request-method :get})
+
+
+(defroutes myapp2
+  (GET ["/file/:name.:ext" :name #".*", :ext #".*"] [name ext]
+       (str "File: " name ext)))
+
+(myapp2 {:uri "/file/thinh.trinh" :request-method :get})
+
+
+(defn -main 
+  "Run server"
+  []
+  (run-server myapp1 {:port 5000}))
+
+
