a597fb4944b937940c23bc1a941c8240f83544cf
ThinhP.Tran <trphthinh@gmail.com>
2017-02-13 20:12:39 +0700

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learncompojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/learncompojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns learncompojure.core-test
+  (:require [clojure.test :refer :all]
+            [learncompojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
